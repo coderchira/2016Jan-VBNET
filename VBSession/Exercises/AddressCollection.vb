a8c95892e963677da47110939937ed2c50e2241d
coderchira <ruchira.aggarwal@gmail.com>
2016-01-14 17:31:03 +0530

    Problem Statements for exercises

---
+++ VBSession/Exercises/AddressCollection.vb
@@ -0,0 +1,11 @@
+﻿Module AddressCollection
+    '3 collections:
+    'collection 1  has names
+    'Collection 2 has emails 
+    'Code such that collection 3 is going to be a collection of collection representing a complete address
+    'e.g:
+    'collection 1  ruchira, ruchi
+    'collection 2  ruchira_agarwal, ruchi_abc
+    'collection 3  col(ruchira, ruchira_agarwal), col(ruchi, ruchi_abc)
+    'For solving this, try exploring the char data type
+End Module
---
+++ VBSession/Exercises/NamesCollection.vb
@@ -0,0 +1,7 @@
+﻿Module NamesCollection
+    '   Create 2 collections
+    '   First collection should have 10 names
+    '   Second collection should be empty
+    '   Code such that : transfer all the names from first collection to another except the one which has your name
+
+End Module
