01bc1b41d880a532926bc3b73924732b3db3c1da
coderchira <ruchira.aggarwal@gmail.com>
2016-01-14 17:51:48 +0530

    solution to access the module level variable

--- VBSession/Day02/AccessSpecifiersDemo.vb
+++ VBSession/Day02/AccessSpecifiersDemo.vb
@@ -4,16 +4,23 @@
     'Public anothername As String
     Public Function DoThis() As Integer
         Dim num As Integer = 10
-        Dim name As String = "dsjfg"
-        name = "sjgdja"
-        Console.WriteLine(name)
+        'the following will not work as we are trying to use local variable with the before it's declared 
+        'name = "Ruby"
+        'to access the module level variable give a fully qualified name using the module's name
+        AccessSpecifiersDemo.name = "Ruby"
+        Console.WriteLine("The value of module level variable is " & AccessSpecifiersDemo.name)
+        Dim name As String = "Rita"
+        Console.WriteLine("The value of the local variable is " & name)
+        DoThisAsWell()
         Return 0
     End Function
 
     Public Function DoThisAsWell() As Boolean
         'scope/visibility of variable is limited to where it is defined
         Dim rtr As Integer = 5
-        name = "dgwe"
+        Console.WriteLine("The value of module level variable is still " & name)
+        name = "Reena"
+        Console.WriteLine("The value of local variable is" & name)
         Return True
     End Function
 
