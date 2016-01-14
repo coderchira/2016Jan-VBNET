28737f4272db5242f4064fd51cb402ade74d18e7
coderchira <ruchira.aggarwal@gmail.com>
2016-01-14 17:53:16 +0530

    solution to access module level variable with the same name as of local variable

--- VBSession/Day02/AccessSpecifiersDemo.vb
+++ VBSession/Day02/AccessSpecifiersDemo.vb
@@ -6,11 +6,15 @@
         Dim num As Integer = 10
         'the following will not work as we are trying to use local variable with the before it's declared 
         'name = "Ruby"
+
         'to access the module level variable give a fully qualified name using the module's name
         AccessSpecifiersDemo.name = "Ruby"
         Console.WriteLine("The value of module level variable is " & AccessSpecifiersDemo.name)
+
         Dim name As String = "Rita"
         Console.WriteLine("The value of the local variable is " & name)
+
+        'Let's call another function to check the module level variable's value
         DoThisAsWell()
         Return 0
     End Function
