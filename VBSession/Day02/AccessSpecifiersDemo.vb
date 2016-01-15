b20cc7a3a70714a4d6a220654a8ad8efd0a3e86a
coderchira <ruchira.aggarwal@gmail.com>
2016-01-15 11:19:07 +0530

    accessing module level variable

--- VBSession/Day02/AccessSpecifiersDemo.vb
+++ VBSession/Day02/AccessSpecifiersDemo.vb
@@ -1,6 +1,8 @@
 ﻿Module AccessSpecifiersDemo
     'module level variable
     Dim name As String
+    'Have a different naming convention for module level variables
+    ' _name ; _number or mNumber; mName etc. to identify them separately.
     'Public anothername As String
     Public Function DoThis() As Integer
         Dim num As Integer = 10
@@ -8,6 +10,7 @@
         'name = "Ruby"
 
         'to access the module level variable give a fully qualified name using the module's name
+        'however, this is not a good practice! So never, do this.
         AccessSpecifiersDemo.name = "Ruby"
         Console.WriteLine("The value of module level variable is " & AccessSpecifiersDemo.name)
 
