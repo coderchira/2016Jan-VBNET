d2b112208314529a91de7c54042f1bc4f7dfe810
coderchira <coderchira@users.noreply.github.com>
2016-01-27 14:56:39 +0530

    Update NamesCollection.vb

--- VBSession/Exercises/NamesCollection.vb
+++ VBSession/Exercises/NamesCollection.vb
@@ -3,5 +3,29 @@
     '   First collection should have 10 names
     '   Second collection should be empty
     '   Code such that : transfer all the names from first collection to another except the one which has your name
+ Dim myColl As New Collection
+    Dim newcoll As New Collection
+    Sub AddCollection()
+        myColl.Add("Priya")
+        myColl.Add("Rekha")
+        myColl.Add("Amit")
+        myColl.Add("Sasmita")
+        myColl.Add("Kishore")
+        myColl.Add("Junaid")
+        myColl.Add("Vani")
+        myColl.Add("Srikant")
+        myColl.Add("Ruchira")
+        myColl.Add("Agnes")
+
+        For Each item In myColl
+            If Not item.Equals("Rekha") Then
+                newcoll.Add(item)
+            End If
+        Next
+        For Each item In newcoll
+            Console.WriteLine(item)
+        Next
+        Console.ReadLine()
+    End Sub
 
 End Module
